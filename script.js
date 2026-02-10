585cf258e3a928cd0bd2ebcc73def11b9680ba97
Krishnang Pandey <firegaming.8768@gmail.com>
2026-02-11 01:34:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 
-var crsr = document.querySelector("#crsr")
-
-
-addEventListener("mousemove", function (dets) {
-    console.log(dets.x)
-    crsr.style.left = dets.x + "px"
-    crsr.style.top = dets.y + "px"
-    // crsr.style.transition = "0.2s"
-})
-
